9470e2420c92fd2fcec11ba4fe86a271b38f1d02
Evgeniy <evgen.ryzhkov@frondevo.com>
2023-06-28 16:30:57 +0300

    cta interactive part 2

--- main.js
+++ main.js
@@ -41,6 +41,46 @@ $(document).ready(function(){
 
      $(".home-cta-form .close-icon, #form-success .button").on("click", function() {
         $(this).closest('.home-cta-form').fadeOut();
-
     });
+
+     $('#customerPhone').on('focus', function(){
+        if (!this.value) {
+          this.value = '+49 ';
+        }
+      });
+
+      $('#customerPhone').mask('+00 0000 0000000', {
+        translation: {
+          '0': { pattern: /[0-9*]/ }
+        }
+      });
+
+    $('.next-button').on('click', function(e) {
+        var $step = $(this).closest('.form-step'),
+            isValid = true;
+
+        // form validation
+        $step.find('input, select').each(function() {
+          if (!this.checkValidity()) {
+            $(this).addClass('invalid');
+            isValid = false;
+          } else {
+            $(this).removeClass('invalid');
+          }
+        });
+
+
+
+        if (!isValid) {
+          return;
+        }
+
+        // Hide current step, show next step
+        $step.hide().next('.form-step').show();
+  });
+
+  $('.prev-button').on('click', function(e) {
+    $(this).closest('.form-step').hide().prev('.form-step').show();
+  });
+
 });
